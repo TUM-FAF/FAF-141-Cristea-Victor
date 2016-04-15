613788084b9a56b205ef5018f0c11e353ff5d1e5
Cristea Victor <cristeavictor0@gmail.com>
2016-04-15 05:07:53 +0300

    Update core.py

--- IDE/Lab-2/core.py
+++ IDE/Lab-2/core.py
@@ -1,217 +1,102 @@
-    def digitClicked(self):
-        clickedButton = self.sender()
-        digitValue = int(clickedButton.text())
+ 
+import sys
+import math
+from PyQt4 import QtCore, QtGui
+from gui import Ui_MainWindow
+ 
+class MyCalc(QtGui.QMainWindow):
+#Instance Vars
+  lcdString = ''      #Stores string for lcd display
+  operation = ''      #Current operation
+  currentNum = 0
+  previousNum = 0
+  ans = 0
 
-        if self.display.text() == '0' and digitValue == 0.0:
-            return
 
-        if self.waitingForOperand:
-            self.display.clear()
-            self.waitingForOperand = False
+  def __init__(self, parent=None):
+    QtGui.QWidget.__init__(self, parent)
+    self.ui = Ui_MainWindow()
+    self.ui.setupUi(self)
+    self.initUI()
+    
+  def initUI(self):
+    #Connect Num Buttons
+    self.connect(self.ui.b1, QtCore.SIGNAL('clicked()'), self.buttonClicked)
+    self.connect(self.ui.b2, QtCore.SIGNAL('clicked()'), self.buttonClicked)
+    self.connect(self.ui.b3, QtCore.SIGNAL('clicked()'), self.buttonClicked)
+    self.connect(self.ui.b4, QtCore.SIGNAL('clicked()'), self.buttonClicked)
+    self.connect(self.ui.b5, QtCore.SIGNAL('clicked()'), self.buttonClicked)
+    self.connect(self.ui.b6, QtCore.SIGNAL('clicked()'), self.buttonClicked)
+    self.connect(self.ui.b7, QtCore.SIGNAL('clicked()'), self.buttonClicked)
+    self.connect(self.ui.b8, QtCore.SIGNAL('clicked()'), self.buttonClicked)
+    self.connect(self.ui.b9, QtCore.SIGNAL('clicked()'), self.buttonClicked)
+    self.connect(self.ui.b0, QtCore.SIGNAL('clicked()'), self.buttonClicked)
+    self.connect(self.ui.period, QtCore.SIGNAL('clicked()'), self.buttonClicked)
+    
+    #Connect Clr
+    self.connect(self.ui.clr, QtCore.SIGNAL('clicked()'), self.clrClicked)
+    
+    #Connect Op buttons
+    self.connect(self.ui.plus, QtCore.SIGNAL('clicked()'), self.opClicked)
+    self.connect(self.ui.minus, QtCore.SIGNAL('clicked()'), self.opClicked)
+    self.connect(self.ui.multiply, QtCore.SIGNAL('clicked()'), self.opClicked)
+    self.connect(self.ui.divide, QtCore.SIGNAL('clicked()'), self.opClicked)
+    self.connect(self.ui.plmin, QtCore.SIGNAL('clicked()'), self.opClicked)
+    self.connect(self.ui.pow, QtCore.SIGNAL('clicked()'), self.opClicked)
+    self.connect(self.ui.sqrt, QtCore.SIGNAL('clicked()'), self.opClicked)
+    
+    #Connect Enter
+    self.connect(self.ui.enter, QtCore.SIGNAL('clicked()'), self.enterClicked)
+    
+  def buttonClicked(self):
+    self.lcdString = self.lcdString + self.sender().text()
+    self.ui.lcd.display(self.lcdString)
+    self.currentNum = float(self.lcdString)
+    
+  def clrClicked(self):
+    #update vars
+    self.lcdString = ''
+    self.currentNum = 0
+    self.previousNum = 0
+    #update display
+    self.ui.lcd.display(0)
+    
+  def opClicked(self):
+    self.previousNum = self.currentNum
+    self.currentNum = 0
+    self.lcdString = ''
+    self.operation = self.sender().objectName()
+    
+  def enterClicked(self):
+    if self.operation == 'plus':
+      self.ans = self.previousNum + self.currentNum
+      
+    if self.operation == 'minus':
+      self.ans = self.previousNum - self.currentNum
+      
+    if self.operation == 'multiply':
+      self.ans = self.previousNum * self.currentNum
+      
+    if self.operation == 'divide':
+      self.ans = self.previousNum / self.currentNum
 
-        self.display.setText(self.display.text() + str(digitValue))
+    if self.operation == 'pow':
+      self.ans = math.pow(self.previousNum, self.currentNum)
 
-    def unaryOperatorClicked(self):
-        clickedButton = self.sender()
-        clickedOperator = clickedButton.text()
-        operand = float(self.display.text())
+    if self.operation == 'sqrt':
+      self.ans = math.sqrt (self.previousNum)
 
-        if clickedOperator == "Sqrt":
-            if operand < 0.0:
-                self.abortOperation()
-                return
-
-            result = math.sqrt(operand)
-            
-        elif clickedOperator == "1/x":
-            if operand == 0.0:
-                self.abortOperation()
-                return
-
-            result = 1.0 / operand
-
-        self.display.setText(str(result))
-        self.waitingForOperand = True
-    def powerOperatorClicked(self):
-        clickedButton = self.sender()
-        clickedOperator = clickedButton.text()
-        operand = float(self.display.text())
-
-        if self.pendingMultiplicativeOperator:
-            if not self.calculate(operand, self.pendingMultiplicativeOperator):
-                self.abortOperation()
-                return
-
-            self.display.setText(str(self.factorSoFar))
-            operand = self.factorSoFar
-            self.factorSoFar = 0.0
-            self.pendingMultiplicativeOperator = ''
-
-        if self.pendingAdditiveOperator:
-            if not self.calculate(operand, pendingAdditiveOperator):
-                self.abortOperation()
-                return
-
-            self.display.setText(str(self.sumSoFar))
-        else:
-            self.sumSoFar = operand
-
-        self.pendingAdditiveOperator = clickedOperator
-        self.waitingForOperand = True
-
-    def additiveOperatorClicked(self):
-        clickedButton = self.sender()
-        clickedOperator = clickedButton.text()
-        operand = float(self.display.text())
-
-        if self.pendingMultiplicativeOperator:
-            if not self.calculate(operand, self.pendingMultiplicativeOperator):
-                self.abortOperation()
-                return
-
-            self.display.setText(str(self.factorSoFar))
-            operand = self.factorSoFar
-            self.factorSoFar = 0.0
-            self.pendingMultiplicativeOperator = ''
-
-        if self.pendingAdditiveOperator:
-            if not self.calculate(operand, pendingAdditiveOperator):
-                self.abortOperation()
-                return
-
-            self.display.setText(str(self.sumSoFar))
-        else:
-            self.sumSoFar = operand
-
-        self.pendingAdditiveOperator = clickedOperator
-        self.waitingForOperand = True
-
-    def multiplicativeOperatorClicked(self):
-        clickedButton = self.sender()
-        clickedOperator = clickedButton.text()
-        operand = float(self.display.text())
-
-        if self.pendingMultiplicativeOperator:
-            if not self.calculate(operand, self.pendingMultiplicativeOperator):
-                self.abortOperation()
-                return
-
-            self.display.setText(str(self.factorSoFar))
-        else:
-            self.factorSoFar = operand
-
-        self.pendingMultiplicativeOperator = clickedOperator
-        self.waitingForOperand = True
-
-    def equalClicked(self):
-        operand = float(self.display.text())
-
-        if self.pendingMultiplicativeOperator:
-            if not self.calculate(operand, self.pendingMultiplicativeOperator):
-                self.abortOperation()
-                return
-
-            operand = self.factorSoFar
-            self.factorSoFar = 0.0
-            self.pendingMultiplicativeOperator = ''
-
-        if self.pendingAdditiveOperator:
-            if not self.calculate(operand, self.pendingAdditiveOperator):
-                self.abortOperation()
-                return
-
-            self.pendingAdditiveOperator = ''
-        else:
-            self.sumSoFar = operand
-
-        self.display.setText(str(self.sumSoFar))
-        self.sumSoFar = 0.0
-        self.waitingForOperand = True
-
-    def pointClicked(self):
-        if self.waitingForOperand:
-            self.display.setText('0')
-
-        if "." not in self.display.text():
-            self.display.setText(self.display.text() + ".")
-
-        self.waitingForOperand = False
-
-    def changeSignClicked(self):
-        text = self.display.text()
-        value = float(text)
-
-        if value > 0.0:
-            text = "-" + text
-        elif value < 0.0:
-            text = text[1:]
-
-        self.display.setText(text)
-
-    def backspaceClicked(self):
-        if self.waitingForOperand:
-            return
-
-        text = self.display.text()[:-1]
-        if not text:
-            text = '0'
-            self.waitingForOperand = True
-
-        self.display.setText(text)
-
-    def clear(self):
-        if self.waitingForOperand:
-            return
-
-        self.display.setText('0')
-        self.waitingForOperand = True
-
-    def clearAll(self):
-        self.sumSoFar = 0.0
-        self.factorSoFar = 0.0
-        self.pendingAdditiveOperator = ''
-        self.pendingMultiplicativeOperator = ''
-        self.display.setText('0')
-        self.waitingForOperand = True
-
-    def clearMemory(self):
-        self.sumInMemory = 0.0
-
-    def readMemory(self):
-        self.display.setText(str(self.sumInMemory))
-        self.waitingForOperand = True
-
-    def setMemory(self):
-        self.equalClicked()
-        self.sumInMemory = float(self.display.text())
-
-    def addToMemory(self):
-        self.equalClicked()
-        self.sumInMemory += float(self.display.text())
-
-    def createButton(self, text, member):
-        button = Button(text)
-        button.clicked.connect(member)
-        return button
-
-    def abortOperation(self):
-        self.clearAll()
-        self.display.setText("####")
-
-    def calculate(self, rightOperand, pendingOperator):
-        
-        if pendingOperator == "+":
-            self.sumSoFar += rightOperand
-        elif pendingOperator == "-":
-            self.sumSoFar -= rightOperand
-        elif pendingOperator == "\327":
-            self.factorSoFar *= rightOperand
-        elif pendingOperator == "\367":
-            if rightOperand == 0.0:
-                return False
-
-            self.factorSoFar /= rightOperand
-        elif pendingOperator == "Pow":
-            self.sumSoFar =math.pow(self.sumSoFar,rightOperand)
-
-        return True
+    if self.operation == 'plmin':
+      self.ans = -self.previousNum 
+      self.ui.lcd.display(self.ans)
+      
+    self.currentNum = self.ans
+    self.ui.lcd.display(self.ans)
+    self.lcdString = ''
+    
+ 
+if __name__ == "__main__":
+  app = QtGui.QApplication(sys.argv)
+  myapp = MyCalc()
+  myapp.show()
+  sys.exit(app.exec_())
